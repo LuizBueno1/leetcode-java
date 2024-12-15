eb42c428d400269677670029169ee2c69cc63bd5
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2024-12-15 09:55:09 -0300

    Add concatenation-of-array

---
+++ 1929-concatenation-of-array/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = new int[]{1, 3, 2, 1};
+        System.out.println(Arrays.toString(s.getConcatenation(nums)));
+    }
+}
---
+++ 1929-concatenation-of-array/src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] getConcatenation(int[] nums){
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[n + i] = nums[i];
+        }
+        return ans;
+    }
+
+}
