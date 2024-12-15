9ce5329f80fe0770fc749cc814b92c8c4cb878d1
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2024-12-14 21:33:03 -0300

    Add number-of-steps-to-reduce-a-number-to-zero

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 123;
+
+        System.out.println(s.numberOfSteps(num));
+
+    }
+}
