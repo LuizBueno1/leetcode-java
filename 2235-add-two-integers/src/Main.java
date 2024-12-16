822830698be3714a68678e547dc87b8e57a23545
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2024-12-16 10:23:32 -0300

    Add add-two-integers

---
+++ 2235-add-two-integers/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num1 = -10;
+        int num2 = 4;
+        System.out.println(s.sum(num1, num2));
+    }
+}
