73ea3232562cfb0472c81debf53883e2d50f7e56
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2024-12-16 10:20:17 -0300

    Add pow

---
+++ 0050-pow/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        double x = 2;
+        int n = -2;
+        System.out.println(s.myPow(x, n));
+    }
+}
