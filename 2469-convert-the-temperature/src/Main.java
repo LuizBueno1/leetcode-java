33c64d9141951798e158cedc85dfdcc16799f818
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2024-12-15 23:15:02 -0300

    Add convert-the-temperature

---
+++ 2469-convert-the-temperature/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        double celsius = 36.50;
+        System.out.println(Arrays.toString(s.convertTemperature(celsius)));
+    }
+}
