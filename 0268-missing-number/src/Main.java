9e709b11aafe77ebbb0d4fd1db1dd0c882d44f3a
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-05-10 17:06:27 -0300

    Add missing-number

---
+++ 0268-missing-number/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] nums = {3, 0, 1};
+
+        System.out.println(s.missingNumber(nums));
+    }
+
+}
